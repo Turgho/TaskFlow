e262c28a8d5ba42182df0ecd54e53d38cf39e60a
Turgho <victor.hugo3692111@gmail.com>
2025-09-18 14:17:22 -0300

    feat: add interfaces to Generic CRUD operations and Unit of Work in services

---
+++ src/TaskFlow.Backend/TaskFlow.Domain/Interfaces/IRepository.cs
@@ -0,0 +1,12 @@
+namespace TaskFlow.Domain.Interfaces;
+
+public interface IRepository<T> where T : class
+{
+    Task<T?> GetByIdAsync(Guid id);
+    Task<IEnumerable<T>> GetAllAsync();
+    Task AddAsync(T entity);
+    void Update(T entity);
+    void Remove(T entity);
+    
+    IQueryable<T> Query(); // útil para filtros e paginação
+}
---
+++ src/TaskFlow.Backend/TaskFlow.Domain/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,7 @@
+namespace TaskFlow.Domain.Interfaces;
+
+public interface IUnitOfWork : IDisposable
+{
+    IRepository<T> Repository<T>() where T : class;
+    Task<int> CommitAsync(CancellationToken cancellationToken = default);
+}
