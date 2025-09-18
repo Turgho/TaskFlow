ad33ef25e2a61f5a4e887a0fe3d69b9791ee6dd8
Turgho <victor.hugo3692111@gmail.com>
2025-09-17 22:44:24 -0300

    feat: DbSet fot AppDbContext.cs

---
+++ src/TaskFlow.Backend/TaskFlow.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using TaskFlow.Domain.Entities;
+
+namespace TaskFlow.Infrastructure.Data;
+
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+{
+    public DbSet<User> Users { get; set; }
+    public DbSet<TodoTask> TodoTasks { get; set; }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+    }
+}
