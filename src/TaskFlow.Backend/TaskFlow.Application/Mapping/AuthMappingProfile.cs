80eaaddcf82599e21739cc4853b0661d99e2b253
Turgho <victor.hugo3692111@gmail.com>
2025-09-18 20:00:17 -0300

    feat: add User -> UserResponse mapping

--- src/TaskFlow.Backend/TaskFlow.Application/Mapping/AuthMappingProfile.cs
+++ src/TaskFlow.Backend/TaskFlow.Application/Mapping/AuthMappingProfile.cs
@@ -1,5 +1,6 @@
 using AutoMapper;
 using TaskFlow.Application.DTOs.Auth;
+using TaskFlow.Application.DTOs.Users;
 using TaskFlow.Domain.Entities;
 
 namespace TaskFlow.Application.Mapping;
@@ -8,10 +9,13 @@ public class AuthMappingProfile : Profile
 {
     public AuthMappingProfile()
     {
-        // Entity -> LoginResponse DTO
+        // Entity -> UserResponseDto
+        CreateMap<User, UserResponseDto>();
+
+        // Entity -> LoginResponseDto
         CreateMap<User, LoginResponseDto>()
             .ForMember(dest => dest.User, opt => opt.MapFrom(src => src))
-            .ForMember(dest => dest.Token, opt => opt.Ignore())       // Token será gerado no AuthService
+            .ForMember(dest => dest.Token, opt => opt.Ignore())
             .ForMember(dest => dest.ExpiresAt, opt => opt.Ignore());
     }
 }
